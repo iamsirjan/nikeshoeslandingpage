e7c50a0874843bd27d7f9eb2435c63f56e42507a
iamsirjan <sirjan.gnr@gmail.com>
2021-04-26 16:55:39 +0545

    home page

---
+++ src/home.js
@@ -0,0 +1,444 @@
+import React from "react";
+import "./style.css";
+
+function home() {
+  return (
+    <div className="home">
+      <div className="navbar">
+        <a href="#" className="logo">
+          Logo
+        </a>
+        <div className="navbar-right menu">
+          <a href="#" className="active">
+            New releases
+          </a>
+          <a href="#">Men</a>
+          <a href="#">Women</a>
+          <a href="#">Kids</a>
+          <a href="#">About</a>
+          <a href="#">Contacts</a>
+        </div>
+        <div className="navbar-right">
+          <a href="#" className="cart">
+            <i className="bx bx-cart-alt"></i>
+            <span class="badge">2</span>
+          </a>
+        </div>
+      </div>
+      <div id="slider" className="slider">
+        <div className="row fullheight slide">
+          <div className="col-6 fullheight">
+            <div className="product-info">
+              <div className="info-wrapper">
+                <div className="product-price left-to-right">
+                  <span>$</span>230
+                </div>
+                <div className="product-name left-to-right">
+                  <h2>Nike ZoomX Vaporfly NEXT%</h2>
+                </div>
+                <div className="product-size left-to-right">
+                  <h3>SIZE</h3>
+                  <div className="size-wrapper">
+                    <div>35</div>
+                    <div>36</div>
+                    <div className="active">37</div>
+                    <div>38</div>
+                    <div>39</div>
+                    <div>40</div>
+                  </div>
+                </div>
+                <div className="product-color left-to-right">
+                  <h3>COLORS</h3>
+                  <div className="color-wrapper">
+                    <div className="active">
+                      <div className="bg-red"></div>
+                    </div>
+                    <div className="">
+                      <div className="bg-blue"></div>
+                    </div>
+                    <div className="">
+                      <div className="bg-white"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="product-description left-to-right">
+                  <p>
+                    The Nike ZoomX Vaporfly NEXT% clears your path to
+                    record-breaking speed with a lighter design and faster feel
+                    than before. With more cushioning underfoot and reduced
+                    weight up top, the result is unprecedented energy return and
+                    comfort
+                  </p>
+                </div>
+                <div className="button mb-5 left-to-right">
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 fullheight img-col">
+            <div className="product-img">
+              <div className="img-wrapper right-to-left">
+                <div className="bounce">
+                  <img
+                    src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoomx-vaporfly-next-running-shoe-4Q5jfG.png?raw=true"
+                    alt="thumbnail image"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="more-images">
+              <div className="more-images-item bottom-up">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoomx-vaporfly-next-running-shoe-4Q5jfG-1.jpg?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+              <div className="more-images-item bottom-up">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoomx-vaporfly-next-running-shoe-4Q5jfG (2).jpg?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+              <div className="more-images-item bottom-up">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoomx-vaporfly-next-running-shoe-4Q5jfG%20(3).jpg?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+              <div className="more-images-item bottom-up">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoomx-vaporfly-next-running-shoe-4Q5jfG%20(4).jpg?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row fullheight slide">
+          <div className="col-6 fullheight">
+            <div className="product-info">
+              <div className="info-wrapper">
+                <div className="product-price left-to-right">
+                  <span>$</span>230
+                </div>
+                <div className="product-name left-to-right">
+                  <h2>Nike Zoom Fly 3</h2>
+                </div>
+                <div className="product-size left-to-right">
+                  <h3>SIZE</h3>
+                  <div className="size-wrapper">
+                    <div>35</div>
+                    <div>36</div>
+                    <div className="active">37</div>
+                    <div>38</div>
+                    <div>39</div>
+                    <div>40</div>
+                  </div>
+                </div>
+                <div className="product-color left-to-right">
+                  <h3>COLORS</h3>
+                  <div className="color-wrapper">
+                    <div className="active">
+                      <div className="bg-red"></div>
+                    </div>
+                    <div className="">
+                      <div className="bg-blue"></div>
+                    </div>
+                    <div className="">
+                      <div className="bg-white"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="product-description left-to-right">
+                  <p>
+                    The Nike ZoomX Vaporfly NEXT% clears your path to
+                    record-breaking speed with a lighter design and faster feel
+                    than before. With more cushioning underfoot and reduced
+                    weight up top, the result is unprecedented energy return and
+                    comfort
+                  </p>
+                </div>
+                <div className="button left-to-right">
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 fullheight img-col">
+            <div className="product-img">
+              <div className="img-wrapper right-to-left">
+                <div className="bounce">
+                  <img
+                    src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoom-fly-3-mens-running-shoe-XhzpPH.png?raw=true"
+                    alt="thumbnail image"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="more-images">
+              <div className="more-images-item bottom-up">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoom-fly-3-mens-running-shoe-XhzpPH.png?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+              <div className="more-images-item bottom-up">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoom-fly-3-mens-running-shoe-XhzpPH%20(1).jpg?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+              <div className="more-images-item bottom-up">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoom-fly-3-mens-running-shoe-XhzpPH%20(2).jpg?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+              <div className="more-images-item bottom-up">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoom-fly-3-mens-running-shoe-XhzpPH%20(3).jpg?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="row fullheight slide">
+            <div className="col-6 fullheight">
+              <div className="product-info">
+                <div className="info-wrapper">
+                  <div className="product-price left-to-right">
+                    <span>$</span>230
+                  </div>
+                  <div className="product-name left-to-right">
+                    <h2>Nike Air Max Alpha TR 3</h2>
+                  </div>
+                  <div className="product-size left-to-right">
+                    <h3>SIZE</h3>
+                    <div className="size-wrapper">
+                      <div>35</div>
+                      <div>36</div>
+                      <div className="active">37</div>
+                      <div>38</div>
+                      <div>39</div>
+                      <div>40</div>
+                    </div>
+                  </div>
+                  <div className="product-color left-to-right">
+                    <h3>COLORS</h3>
+                    <div className="color-wrapper">
+                      <div className="active">
+                        <div className="bg-red"></div>
+                      </div>
+                      <div className="">
+                        <div className="bg-blue"></div>
+                      </div>
+                      <div className="">
+                        <div className="bg-white"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="product-description left-to-right">
+                    <p>
+                      The Nike ZoomX Vaporfly NEXT% clears your path to
+                      record-breaking speed with a lighter design and faster
+                      feel than before. With more cushioning underfoot and
+                      reduced weight up top, the result is unprecedented energy
+                      return and comfort
+                    </p>
+                  </div>
+                  <div className="button left-to-right">
+                    <button>Add to cart</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-6 fullheight img-col">
+              <div className="product-img">
+                <div className="img-wrapper right-to-left">
+                  <div className="bounce">
+                    <img
+                      src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-max-alpha-tr-3-mens-training-shoe-0C1CV7.png?raw=true"
+                      alt="thumbnail image"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="more-images">
+                <div className="more-images-item bottom-up">
+                  <img
+                    src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-max-alpha-tr-3-mens-training-shoe-0C1CV7.png?raw=true"
+                    alt="thumbnail image"
+                  />
+                </div>
+                <div className="more-images-item bottom-up">
+                  <img
+                    src=" https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-max-alpha-tr-3-mens-training-shoe-0C1CV7%20(1).jpg?raw=true"
+                    alt="thumbnail image"
+                  />
+                </div>
+                <div className="more-images-item bottom-up">
+                  <img
+                    src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-max-alpha-tr-3-mens-training-shoe-0C1CV7%20(2).jpg?raw=true"
+                    alt="thumbnail image"
+                  />
+                </div>
+                <div className="more-images-item bottom-up">
+                  <img
+                    src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-max-alpha-tr-3-mens-training-shoe-0C1CV7%20(3).jpg?raw=true"
+                    alt="thumbnail image"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="row fullheight slide">
+              <div className="col-6 fullheight">
+                <div className="product-info">
+                  <div className="info-wrapper">
+                    <div className="product-price left-to-right">
+                      <span>$</span>230
+                    </div>
+                    <div className="product-name left-to-right">
+                      <h2>Nike Air Zoom SuperRep</h2>
+                    </div>
+                    <div className="product-size left-to-right">
+                      <h3>SIZE</h3>
+                      <div className="size-wrapper">
+                        <div>35</div>
+                        <div>36</div>
+                        <div className="active">37</div>
+                        <div>38</div>
+                        <div>39</div>
+                        <div>40</div>
+                      </div>
+                    </div>
+                    <div className="product-color left-to-right">
+                      <h3>COLORS</h3>
+                      <div className="color-wrapper">
+                        <div className="active">
+                          <div className="bg-red"></div>
+                        </div>
+                        <div className="">
+                          <div className="bg-blue"></div>
+                        </div>
+                        <div className="">
+                          <div className="bg-white"></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="product-description left-to-right">
+                      <p>
+                        The Nike ZoomX Vaporfly NEXT% clears your path to
+                        record-breaking speed with a lighter design and faster
+                        feel than before. With more cushioning underfoot and
+                        reduced weight up top, the result is unprecedented
+                        energy return and comfort
+                      </p>
+                    </div>
+                    <div className="button left-to-right">
+                      <button>Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-6 fullheight img-col">
+                <div className="product-img">
+                  <div className="img-wrapper right-to-left">
+                    <div className="bounce">
+                      <img
+                        src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-zoom-superrep-mens-hiit-class-shoe-ZWLnJW.jpg?raw=true"
+                        alt="thumbnail image"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="more-images">
+                  <div className="more-images-item bottom-up">
+                    <img
+                      src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-zoom-superrep-mens-hiit-class-shoe-ZWLnJW%20(1).png?raw=true"
+                      alt="thumbnail image"
+                    />
+                  </div>
+                  <div className="more-images-item bottom-up">
+                    <img
+                      src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-zoom-superrep-mens-hiit-class-shoe-ZWLnJW%20(2).jpg?raw=true"
+                      alt="thumbnail image"
+                    />
+                  </div>
+                  <div className="more-images-item bottom-up">
+                    <img
+                      src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-zoom-superrep-mens-hiit-class-shoe-ZWLnJW%20(3).jpg?raw=true"
+                      alt="thumbnail image"
+                    />
+                  </div>
+                  <div className="more-images-item bottom-up">
+                    <img
+                      src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-zoom-superrep-mens-hiit-class-shoe-ZWLnJW%20(4).jpg?raw=true"
+                      alt="thumbnail image"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div id="slide-control" className="slide-control">
+              <div className="slide-control-item">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoomx-vaporfly-next-running-shoe-4Q5jfG.png?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+              <div className="slide-control-item">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/zoom-fly-3-mens-running-shoe-XhzpPH.png?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+              <div className="slide-control-item">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-max-alpha-tr-3-mens-training-shoe-0C1CV7.png?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+              <div className="slide-control-item">
+                <img
+                  src="https://github.com/trananhtuat/nike-shoes-landing-page/blob/main/assets/air-zoom-superrep-mens-hiit-class-shoe-ZWLnJW (1).png?raw=true"
+                  alt="thumbnail image"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="modal" class="modal">
+        <span id="modal-close" class="close">
+          &times;
+        </span>
+        <img id="modal-content" class="modal-content" />
+        <div class="more-images">
+          <div class="more-images-item">
+            <img class="img-preview" />
+          </div>
+          <div class="more-images-item">
+            <img class="img-preview" />
+          </div>
+          <div class="more-images-item">
+            <img class="img-preview" />
+          </div>
+          <div class="more-images-item">
+            <img class="img-preview" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default home;
